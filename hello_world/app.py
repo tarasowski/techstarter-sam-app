75c3325cd81f7ecd3e9c698febe3df46122ada30
tarasowski <dimitri+github@tarasowski.de>
2023-12-04 13:00:42 +0100

    fix: fixed testing error

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello Dimi!",
+            "message": "hello world",
             # "location": ip.text.replace("\n", "")
         }),
     }
